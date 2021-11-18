e19dfa1b6b466b222747df58f930dabd271c8d3c
PotatoPlan <linzeshu1998@hotmail.com>
2021-11-18 22:20:04 +1100

    GET first page of data (20 characters)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Characters from "./components/Characters";
 function App() {
   return (
     <div>
+      <div className="title-bar">
+        <h1>Rick and Morty All Human Characters</h1>
+      </div>
+
       <Characters />
     </div>
   );
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Characters.css";
 
 export default function Characters() {
+  // Base URL
+  const baseUrl = "https://rickandmortyapi.com/api";
+
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/character/?species=human`).then((response) => {
+      // Check response data
+      console.log(response.data);
+      setCharacters(response.data.results);
+    });
+  }, []);
+
+  if (!characters) {
+    return null;
+  }
+
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="characters-container">
+      {characters &&
+        characters.map((character, index) => <h6>{character.name}</h6>)}
     </div>
   );
 }
